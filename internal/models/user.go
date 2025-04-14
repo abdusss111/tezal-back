bcefe94c3f3393045d61eaecb4b6886c330847a2
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-04-14 18:27:53 +0500

    myau

--- internal/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID        int    `db:"id" json:"id"`
-	Name      string `db:"name" json:"name"`
-	Email     string `db:"email" json:"email"`
-	Age       int    `db:"age" json:"age"`
-	CreatedAt string `db:"created_at" json:"created_at"`
-}
